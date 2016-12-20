d9577336ba5b6a0c54105324a11a3d1155d9f55c
Rafael Gomes Dantas <rafagd@gmail.com>
2016-12-20 14:57:05 +0000

    Removing bank and refactoring

--- bot.py
+++ bot.py
@@ -1,8 +1,7 @@
 import logging
 import re
+import random
 import log
-
-from bank import PhraseBank 
 
 logger = logging.getLogger("Bot")
 
@@ -17,19 +16,53 @@ class Bot:
     def listen(self, person, message):
         # Receives an escaped version of the message, and the number of times
         # it replaced the name of the bot for {nome}
-        message, subs = regexp.subn("{nome}", message.replace("{", "{{").replace("}", "}}"))
-        message = log.Message(message, self.log.size + 1)
+        message = message.replace("{", "{{").replace("}", "}}")
+        m, subs = self.regexp.subn("{nome}", message)
+        message = log.Message(m, self.log.size + 1)
         self.log.add(message)
 
         response = ""
         if subs > 0:
-            response = self.reply(person, message)
+            response = self.reply(message).text.format(nome=person)
+        elif random.random() < 0.01:
+            response = self.reply(message).text.format(nome=person)
 
         self.index.update(message)
         return response
 
 
-    def reply(self, person, message):
-        return self.bank.read_weighted(message).format(nome=person)
+    def reply(self, message):
+        if random.random() < 0.05:
+            return self.reply_random()
+        return self.reply_message(message)
 
+
+    def reply_random(self):
+        return self.log.read_message(int(random.random() * self.log.size))
+
+
+    def reply_word(self, word):
+        offsets = self.index.offsets(word)
+        return self.log.read_message(offsets[int(random.random() * len(offsets))])
+
+
+    def reply_message(self, message):
+        scale_sum = 0
+        for word in message.components:
+            scale = self.index.scale(word)
+            if scale > 0:
+                scale_sum += 1.0 / scale
+            
+        selected = random.random() * scale_sum
+        for word in message.components:
+            scale = self.index.scale(word)
+            if scale > 0:
+                scale_sum -= 1.0 / scale
+            if selected > scale_sum:
+                return self.reply_word(word)
+        
+        # Unreachable?
+        return self.reply_random()
+
+            
 
--- log.py
+++ log.py
@@ -1,25 +1,28 @@
 import os
 import re
 
-SEPARATORS = re.compile("[ ,.!?\"':\\[\\]\n\r\t]+")
+SEPARATORS = re.compile('[ ,.!?\"\':\\[\\]\n\r\t]+')
 
 class Message:
     def __init__(self, text, offset):
         self.text       = text
         self.offset     = offset
-        self.components = SEPARATORS.split(text.upper())
+        self.components = list()
+        for word in SEPARATORS.split(text.upper()):
+            if word != '':
+                self.components.append(word)
 
 
 class Log:
     def __init__(self, file_path):
-        self.file = open(file_path, "ab+")
+        self.file = open(file_path, 'ab+')
         self.file.seek(0, os.SEEK_END)
         self.size = self.file.tell()
 
 
     def add(self, message):
         self.file.seek(0, os.SEEK_END)
-        self.file.write(str.encode(message.text) + b"\x0A")
+        self.file.write(str.encode(message.text) + b'\x0A')
         self.size = self.file.tell()
 
 
@@ -35,7 +38,7 @@ class Log:
     def read_message(self, position):
         self.file.seek(position)
 
-        while self.file.read(1) != b"\x0A":
+        while self.file.read(1) != b'\x0A':
             position -= 1
 
             if position < 0:
@@ -50,29 +53,45 @@ class Log:
 
 class Index:
     def __init__(self, log):
-        self.scales  = dict()
-        self.offsets = dict()
+        self._scales  = dict()
+        self._offsets = dict()
         for message in log.iterator():
             self.update(message)
 
 
-    def update(self, message)
+    def update(self, message):
         for word in message.components:
             self.update_scales(word)
             self.update_offsets(word, message.offset)
 
 
+    # Assumes word is uppercase
     def update_scales(self, word):
         try:
-            self.scales[word] += 1
+            self._scales[word] = self._scales[word] + 1
         except:
-            self.scales[word] = 1
+            self._scales[word] = 1
 
 
+    # Assumes word is uppercase
     def update_offsets(self, word, offset):
         try:
-            self.offsets[word].append(offset)
+            self._offsets[word].append(offset)
         except:
-            self.offsets[word] = list(offset)
+            self._offsets[word] = [ offset ]
 
-    
+
+    def offsets(self, word):
+        word = word.upper()
+        try:
+            return self._offsets[word]
+        except:
+            return list()
+
+
+    def scale(self, word):
+        word = word.upper()
+        try:
+            return self._scales[word]
+        except:
+            return 0
